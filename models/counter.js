bb05a2cecb93657ced09a12023b98fd433c8c435
aman <amanm.ug23.cs@nitp.ac.in>
2024-11-02 00:34:11 +0530

    added

--- models/counter.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CounterSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  seq: { type: Number, default: 0 },
-});
-
-const Counter = mongoose.model('Counter', CounterSchema);
-module.exports = Counter;
